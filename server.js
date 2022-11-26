7a960c1454ee1dd53701db9fb930f397e0af5181
史开乐 <csurfssecure@gmail.com>
2022-11-26 16:59:26 -0500

    MYSQL routes

--- server.js
+++ server.js
@@ -23,9 +23,42 @@ const db = mysql.createConnection(
 
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);    
+//database query for all rows/candidates
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);     
+// });
+
+//database query for single row/candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(row);
 });
+
+//database query to delete single row/candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(results);
+// });
+
+// database query to create single row/candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, results) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(results);
+});
+
+
 // catchcall route, must go at bottom of previous calls 
 app.use((req, res) => {
     res.status(404).end();
