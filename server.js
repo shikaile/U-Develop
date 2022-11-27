540a981df0a4933170ab1c0d73c52512957857eb
史开乐 <csurfssecure@gmail.com>
2022-11-26 20:34:42 -0500

    CRUD routes

--- server.js
+++ server.js
@@ -25,38 +25,95 @@ const db = mysql.createConnection(
 
 //database query for all rows/candidates
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);     
-// });
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+    db.query(sql,(err, rows) => {
+    if (err) {
+        res.status(500).json({error: err.message});
+        return;
+    }     
+    res.json({
+        message: 'sucess',
+        data: rows
+        });
+    });
+});
 
 //database query for single row/candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+app.get('/api/candidate/:id', (req,res) => {
+    const sql =`SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
     if (err){
-        console.log(err);
+        res.status(400).json({error: err.message});
+        return;
     }
-    console.log(row);
+    res.json({
+        message:'sucess',
+        data: row
+        });
+    });
 });
 
 //database query to delete single row/candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, results) => {
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+    res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+    res.json({
+        message: 'Candidate not found'
+    });
+    } else {
+    res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+            });
+        }
+    });
+});
+
+app.post('/api/candidate', ({body}, res) => {
+    const errors = inputCheck(body,'first_name', 'last_name', 'industry_connected');
+
+    if (errors){
+        res.status(400).json({error: errors});
+        return;
+    }
+    
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'sucess',
+            data: body
+            });
+    });
+});
+
+// database query to create single row/candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, results) => {
 //     if (err){
 //         console.log(err);
 //     }
 //     console.log(results);
 // });
-
-// database query to create single row/candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-    VALUES (?,?,?,?)`;
-
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, results) => {
-    if (err){
-        console.log(err);
-    }
-    console.log(results);
-});
 
 
 // catchcall route, must go at bottom of previous calls 
