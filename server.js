6a18aebf0ec85cd2c71784d212eafa103851a594
史开乐 <csurfssecure@gmail.com>
2022-11-27 11:34:06 -0500

    party management  api calls

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/api/candidate/:id', (req,res) => {
     LEFT JOIN parties 
     ON candidates.party_id = parties.id 
     WHERE candidates.id = ?`;
-    
+
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -64,28 +64,6 @@ app.get('/api/candidate/:id', (req,res) => {
         message:'sucess',
         data: row
         });
-    });
-});
-
-//database query to delete single row/candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-    res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-    res.json({
-        message: 'Candidate not found'
-    });
-    } else {
-    res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-            });
-        }
     });
 });
 
@@ -113,6 +91,114 @@ app.post('/api/candidate', ({body}, res) => {
             });
     });
 });
+
+//api route to get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'sucess',
+            data: rows
+        });
+    });
+});
+
+//api call for single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'sucess',
+            data: row
+        });
+    });
+});
+
+app.put('/api/candidate/:id', (req, res) =>{
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors){
+        res.status(400).json({ error: errors});
+        return;
+    }
+    const sql = 
+        `UPDATE candidates SET party_id = ?
+        WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({ error: err.message});
+            //check if record found
+        } else if (!result.affectedRows){
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'sucess',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+//database query to delete single row/candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+    res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+    res.json({
+        message: 'Candidate not found'
+    });
+    } else {
+    res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+            });
+        }
+    });
+});
+
+//database query to delete party from candidate info
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+    res.statusMessage(400).json({ error: res.message });
+    //checks if anyhting was deleted/affected
+    } else if (!result.affectedRows) {
+    res.json({
+        message: 'Party not found'
+    });
+    } else {
+    res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+            });
+        }
+    });
+});
+
 
 // catchcall route, must go at bottom of previous calls 
 app.use((req, res) => {
